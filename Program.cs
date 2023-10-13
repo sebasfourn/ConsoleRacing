55b54f8e98e3274e9a16c29c66151d02e81ad344
sebastien fournier <sfournier.qc@gmail.com>
2023-10-13 13:11:52 -0400

    m

--- Program.cs
+++ Program.cs
@@ -11,6 +11,28 @@ Console.WriteLine($"Name: {calendar[0].Name}, Date: {calendar[0].Date}, City: {c
 Console.WriteLine($"Name: {calendar[1].Name}, Date: {calendar[1].Date}, City: {calendar[1].City}");
 Console.WriteLine($"Name: {calendar[2].Name}, Date: {calendar[2].Date}, City: {calendar[2].City}");
 
-// var driver1 = new Driver("Ryan Tuerck");
-// race1.AddDriver(driver1);
-// Console.WriteLine(race1);
+var driver1 = new Driver("Ryan Tuerck");
+var driver2 = new Driver("Chelsea DeNofa");
+var driver3 = new Driver("Simen Olsen");
+var driver4 = new Driver("Matt Field");
+var driver5 = new Driver("Frederic Aasbo");
+var driver6 = new Driver("Aurimas Bakchis");
+var driver7 = new Driver("James Dean");
+var driver8 = new Driver("Chris Forsberg");
+var driver9 = new Driver("Dylan Hughes");
+var driver10 = new Driver("Kazuya Taguchi");
+
+race1.AddDriver(driver1);
+race1.AddDriver(driver2);
+race1.AddDriver(driver3);
+race1.AddDriver(driver4);
+race1.AddDriver(driver5);
+race1.AddDriver(driver6);
+race1.AddDriver(driver7);
+race1.AddDriver(driver8);
+race1.AddDriver(driver9);
+race1.AddDriver(driver10);
+
+// Console.WriteLine($"The 1st driver on the waiting list is: {waitingList[0]}");
+
+race1.RemoveDriver(driver1);
--- Race.cs
+++ Race.cs
@@ -11,10 +11,32 @@ public class Race
     City = city;
   }
 
-  // public void AddDriver(Driver driver)
-  // {
-  //   Race.add(driver);
-  // }
+  public List<Driver> drivers = new List<Driver>();
+  public Queue<Driver> waitingList = new Queue<Driver>();
+
+  public void AddDriver(Driver driver)
+  {
+    if (drivers.Count < 5)
+    {
+      drivers.Add(driver);
+      Console.WriteLine($"{driver.Name} was successfully added to the race");
+    }
+    else
+    {
+      waitingList.Enqueue(driver);
+      Console.WriteLine($"{driver.Name} was successfully added to the waiting list");
+    }
+  }
+
+  public void RemoveDriver(Driver driver)
+  {
+    drivers.Remove(driver);
+    waitingList.Dequeue();
+    foreach (Driver drive in drivers)
+    {
+      Console.WriteLine(drive.Name);
+    }
+  }
 }
 
 public class Driver
