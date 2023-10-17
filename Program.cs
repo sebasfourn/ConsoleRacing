31dc3994fa51296d5bc6f3ceb9d03c5f1e76cf6e
sebastien fournier <sfournier.qc@gmail.com>
2023-10-17 17:36:44 -0400

    finishing racing project

--- Program.cs
+++ Program.cs
@@ -2,35 +2,14 @@
 var race2 = new Race("Road Atlanta");
 var race3 = new Race("Orlando Speed World");
 
-var races = new List<Race>() { race1, race2, race3 }
-races.Add(race1);
-races.Add(race2);
-races.Add(race3);
-
-PrintRaces();
-foreach (Race race in races)
-{
-  Console.WriteLine($"{races.IndexOf(race) + 1}. {race.Name}");
-}
-
-PrintMenu();
-{
-  Console.WriteLine("1. See the drivers");
-  Console.WriteLine("2. See the waiting list")
-  Console.WriteLine("3. Add a driver to the race");
-  Console.WriteLine("4. Remove a driver from the race");
-  Console.WriteLine("5. Go back");
-}
+var races = new List<Race>() { race1, race2, race3};
 
 int choice = 0;
-
 do
 {
-  Console.WriteLine("1. See all races");
-  Console.WriteLine("2. Select a race");
-  Console.WriteLine("3. Quit")
-  string choiceString = Console.ReadLine();
-  int choice = int.Parse(choiceString);
+  PrintMenu();
+  string? choiceString = Console.ReadLine();
+  choice = int.Parse(choiceString ?? "0");
 
   switch (choice)
   {
@@ -39,71 +18,131 @@ do
     break;
 
     case 2:
-      PrintRaces();
-      string raceChoiceString = Console.ReadLine();
-      int raceChoice = int.Parse(raceChoiceString);
-
-      int raceChoice = raceChoice switch
-      {
-        1 => race1
-        2 => race2
-        3 => race3
-      }
-
-      int userChoice = 0;
-
+      int raceChoice = 0;
       do
       {
-        PrintMenu();
-        string userChoiceString = Console.ReadLine();
-        int userChoice = int.Parse(userChoiceString);
+        PrintRaces();
+        Console.WriteLine("4. Go back");
+        string? raceChoiceString = Console.ReadLine();
+        raceChoice = int.Parse(raceChoiceString ?? "0");
 
-        switch (userChoice)
+        if (raceChoice == 1 || raceChoice == 2 || raceChoice == 3)
         {
-          case 1:
+          Race race = raceChoice switch
+          {
+            1 => race1,
+            2 => race2,
+            3 => race3,
+            _ => race1
+          };
 
-          break;
+          int userChoice = 0;
+          do
+          {
+            PrintOptions();
+            string? userChoiceString = Console.ReadLine();
+            userChoice = int.Parse(userChoiceString ?? "0");
 
-          case 2:
-          break;
+            switch (userChoice)
+            {
+              case 1:
+                // race.Drivers.Any() ? PrintDrivers(race) : PrintNoDrivers();
 
-          case 3:
-          break;
+                if (race.Drivers.Any())
+                {
+                  PrintDrivers(race);
+                }
+                else
+                {
+                  PrintNoDrivers();
+                }
+              break;
 
-          case 4:
-          break;
+              case 2:
+                  if (race.WaitingList.Any())
+                {
+                  PrintWaitingList(race);
+                }
+                else
+                {
+                  PrintNoDrivers();
+                }
+              break;
 
-          default:
-            Console.WriteLine("Wrong option!");
-          break:
+              case 3:
+                Console.WriteLine("What's the driver name?");
+                string? driverName = Console.ReadLine();
+                var driver = new Driver(driverName ?? "0");
+                race.AddDriver(driver);
+              break;
+
+              case 4:
+                if (race.Drivers.Any())
+                {
+                  Console.WriteLine("Choose a driver:");
+                  PrintDrivers(race);
+                  string? driverChoiceString = Console.ReadLine();
+                  int driverChoice = int.Parse(driverChoiceString ?? "0");
+                  race.RemoveDriver(driverChoice - 1);
+                }
+                else
+                {
+                  PrintNoDrivers();
+                }
+              break;
+            }
+          } while (userChoice != 5);
         }
-      } while (userChoice != 5);
-    break;
-
-    default:
-      Console.WriteLine("Wrong option!")
+      } while (raceChoice != 4);
     break;
   }
 } while (choice != 3);
 
-// var driver1 = new Driver("Ryan Tuerck");
-// var driver2 = new Driver("Chelsea DeNofa");
-// var driver3 = new Driver("Simen Olsen");
-// var driver4 = new Driver("Matt Field");
-// var driver5 = new Driver("Frederic Aasbo");
-// var driver6 = new Driver("Aurimas Bakchis");
-// var driver7 = new Driver("James Dean");
-// var driver8 = new Driver("Chris Forsberg");
-// var driver9 = new Driver("Dylan Hughes");
-// var driver10 = new Driver("Kazuya Taguchi");
+// Functions
 
-// race1.AddDriver(driver1);
-// race1.AddDriver(driver2);
-// race1.AddDriver(driver3);
-// race1.AddDriver(driver4);
-// race1.AddDriver(driver5);
-// race1.AddDriver(driver6);
-// race1.AddDriver(driver7);
-// race1.AddDriver(driver8);
-// race1.AddDriver(driver9);
-// race1.AddDriver(driver10);
+void PrintMenu()
+{
+  Console.WriteLine("Choose an option:");
+  Console.WriteLine("1. See all races");
+  Console.WriteLine("2. Select a race");
+  Console.WriteLine("3. Quit");
+}
+
+void PrintRaces()
+{
+  foreach (Race race in races)
+  {
+    Console.WriteLine($"{races.IndexOf(race) + 1}. {race.Name}");
+  }
+}
+
+void PrintOptions()
+{
+  Console.WriteLine("Choose an option:");
+  Console.WriteLine("1. See the drivers");
+  Console.WriteLine("2. See the waiting list");
+  Console.WriteLine("3. Add a driver to the race");
+  Console.WriteLine("4. Remove a driver from the race");
+  Console.WriteLine("5. Go back");
+}
+
+void PrintDrivers(Race race)
+{
+  foreach (Driver driver in race.Drivers)
+  {
+    Console.WriteLine($"{race.Drivers.IndexOf(driver) + 1}. {driver.Name}");
+  }
+}
+
+void PrintWaitingList(Race race)
+{
+  foreach (Driver driver in race.WaitingList)
+  {
+    Console.WriteLine($"{driver.Name}");
+  }
+}
+
+void PrintNoDrivers()
+{
+  Console.WriteLine("No drivers yet!");
+}
--- Race.cs
+++ Race.cs
@@ -2,13 +2,13 @@ public class Race
 {
   public string Name { get; set; }
   public List<Driver> Drivers { get; set; }
-  public Queue<Driver> WaitingList { get; set; }
+  public List<Driver> WaitingList { get; set; }
 
-  public Race(string name, string date, string city)
+  public Race(string name)
   {
     Name = name;
     Drivers = new List<Driver>();
-    WaitingList = new Queue<Driver>();
+    WaitingList = new List<Driver>();
   }
 
   public void AddDriver(Driver driver)
@@ -16,18 +16,27 @@ public class Race
     if (Drivers.Count < 2)
     {
       Drivers.Add(driver);
-      Console.WriteLine($"{driver.Name} was successfully added to the race");
+      Console.WriteLine($"{driver.Name} was successfully added to the race!");
     }
     else
     {
-      WaitingList.Enqueue(driver);
-      Console.WriteLine($"{driver.Name} was successfully added to the waiting list");
+      WaitingList.Add(driver);
+      Console.WriteLine($"{driver.Name} was successfully added to the waiting list!");
     }
   }
 
-  public void RemoveDriver(Driver driver)
+  public void RemoveDriver(int driverIndex)
   {
+    Driver driver = Drivers[driverIndex];
     Drivers.Remove(driver);
-    WaitingList.Dequeue();
+    Console.WriteLine($"{driver.Name} was successfully removed from the race!");
+
+    DequeueWaitingList();
+  }
+  public void DequeueWaitingList()
+  {
+    Driver driver = WaitingList[0];
+    WaitingList.Remove(driver);
+    AddDriver(driver);
   }
 }
